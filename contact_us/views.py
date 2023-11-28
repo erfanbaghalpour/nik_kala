ae43fa84322e3785c51f90e60904f447ee687737
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-11-28 20:57:10 +0330

    Class-based view 2

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 
 from .forms import ContactUsForm
 from .models import ContactUs
 from .forms import *
 
 
-class ContactUsView(View):
-    def get(self, request):
-        contact_form = ContactUsModelForm()
-        context = {
-            'contact_form': contact_form
-        }
-        return render(request, 'contact_us/contact_us.html', context=context)
-
-    def post(self, request):
-        contact_form = ContactUsModelForm(request.POST)
-        if contact_form.is_valid():
-            contact_form.save()
-            return redirect('core:index')
-        context = {
-            'contact_form': contact_form
-        }
-        return render(request, 'contact_us/contact_us.html', context=context)
+# class ContactUsView(View):
+#     def get(self, request):
+#         contact_form = ContactUsModelForm()
+#         context = {
+#             'contact_form': contact_form
+#         }
+#         return render(request, 'contact_us/contact_us.html', context=context)
+#
+#     def post(self, request):
+#         contact_form = ContactUsModelForm(request.POST)
+#         if contact_form.is_valid():
+#             contact_form.save()
+#             return redirect('core:index')
+#         context = {
+#             'contact_form': contact_form
+#         }
+#         return render(request, 'contact_us/contact_us.html', context=context)
 
 # def contact_us_view(request):
 #     if request.method == 'POST':
@@ -49,3 +50,13 @@ class ContactUsView(View):
 #         'contact_form': contact_form
 #     }
 #     return render(request, 'contact_us/contact_us.html', context=context)
+
+
+class ContactUsView(FormView):
+    template_name = 'contact_us/contact_us.html'
+    form_class = ContactUsModelForm
+    success_url = '/contact-us/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
--- product/views.py
+++ product/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, DetailView
+
 from .models import Product
 from django.http import Http404
 
@@ -13,3 +15,15 @@ def product_detail(request, slug):
         'product_detail': product_detail
     }
     return render(request, 'product/product_detail.html', context=context)
+
+
+class ProductDetailView(DetailView):
+    template_name = 'product/product_detail.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(ProductDetailView, self).get_context_data()
+    #     slug = kwargs['slug']
+    #     product_detail = get_object_or_404(Product, slug=slug)
+    #     context['product'] = product_detail
+    #     return context
+    model = Product
