21a0bc55ec9c4d579025021eff55d15d0578090d
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-12-16 20:40:18 +0330

    Blog's model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url_title', 'parent', 'is_active']
+    list_editable = ['url_title', 'is_active', 'parent']
+
+
+admin.site.register(models.BlogCategory, BlogCategoryAdmin)
+admin.site.register(models.Blog)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class BlogCategory(models.Model):
+    parent = models.ForeignKey('BlogCategory', on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    url_title = models.CharField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    # class Meta:
+    #     verbose_name = ''
+    #     verbose_name_plural = ''
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=400, db_index=True, allow_unicode=True)
+    image = models.ImageField(max_length=1000, upload_to='images/blogs')
+    short_description = models.CharField(max_length=1000)
+    text = models.TextField()
+    is_active = models.BooleanField(default=True)
+    author = models.CharField(max_length=300, null=True, blank=True)
+    author_image = models.ImageField(max_length=1000, upload_to='images/blogs', null=True, blank=True)
+    selected_categories = models.ManyToManyField(BlogCategory)
+
+    def __str__(self):
+        return self.title
+
+    # class Meta:
+    #     verbose_name = ''
+    #     verbose_name_plural = ''
