9cc3d665cc44ae586eec0d7c88f7f575493e5628
Erfan Baghalpour <erfanbaghalpour@gmail.com>
2023-12-23 21:59:53 +0330

    Blog detail

--- blog/urls.py
+++ blog/urls.py
@@ -13,6 +13,8 @@ from . import views
 urlpatterns = [
     # path('', views.blog_view, name="index_blog"),
     path('all-posts/', views.posts, name="post_blog"),
-    path('detail/<slug:slug>/', views.single_post, name="single_post"),
+    # path('detail/<slug:slug>/', views.single_post, name="single_post"),
     path('', views.BlogListView.as_view(), name="index_blog"),
+    path('detail/<int:pk>/', views.BlogDetailView.as_view(), name="single_blog"),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
-from .models import Blog
+from django.views.generic import DetailView
+
+from .models import Blog, BlogCategory
 from django.views.generic.list import ListView
 from jalali_date import datetime2jalali, date2jalali
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def blog_view(request: HttpRequest):
@@ -29,10 +32,37 @@ class BlogView(View):
 
 class BlogListView(ListView):
     model = Blog
-    paginate_by = 4
+    # paginate_by = 4
     template_name = 'blog/blog.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super(BlogListView, self).get_context_data(*args, **kwargs)
-        context['date'] = datetime2jalali(self.request.user.date_joined).strftime('%y/%m/%d _ %H:%M:%S')
+        # context['date'] = datetime2jalali(self.request.user.date_joined).strftime('%y/%m/%d _ %H:%M:%S')
+        # paginator = Paginator(context['object_list'], 10)
+        # page = self.request.GET.get('page')
+        # try:
+        #     paginated_objects = paginator.page(page)
+        # except PageNotAnInteger:
+        #     paginated_objects = paginator.page(1)
+        # except EmptyPage:
+        #     paginated_objects = paginator.page(paginator.num_pages)
+        # context['paginated_objects'] = paginated_objects
         return context
+
+
+def blog_categories_partial(request: HttpRequest):
+    blog_main_categories = BlogCategory.objects.filter(is_active=True, parent_id=None)
+    context = {
+        'main_categories': blog_main_categories
+    }
+    return render(request, 'blog', context=context)
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/detail-post.html'
+
+    def get_queryset(self):
+        query = super(BlogDetailView, self).get_queryset()
+        query = query.filter(is_active=True)
+        return query
